4f33ff2034adf2c76efd8b9eb1150dd9d0af3a4a
maxmaxjian <jianw812@gmail.com>
2016-11-20 23:38:40 -0500

    first edit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+
+class solution {
+public:
+    std::vector<pair<int,int>> getSkyline(const std::vector<std::vector<int>> & buildings) {
+	
+    }
+};
+
+int main() {
+    
+}
