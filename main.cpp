9e9fece471541c40adae558b5f7fc7a8a3b18957
maxmaxjian <jianw812@gmail.com>
2016-11-20 23:44:04 -0500

    first edit

--- main.cpp
+++ main.cpp
@@ -10,5 +10,15 @@ public:
 };
 
 int main() {
+    std::vector<std::vector<int>> buildings{
+	{2, 9, 10},
+	{3, 7, 15},
+	{5, 12, 12},
+	{15, 20, 10},
+	{19, 24, 8}
+    };
+
+    solution soln;
+    auto skyline = soln.getSkyline(buildings);
     
 }
